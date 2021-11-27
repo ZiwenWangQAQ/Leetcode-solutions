896f11ac857bf023a859eedce3144afd50ea281c
ZiwenWangQAQ <95108867+ZiwenWangQAQ@users.noreply.github.com>
2021-11-27 08:45:14 +0800

    Update 1. Two Sum.py
    adding key-value in dictionary while loop runs faster and use less storage than checking after creating whole dictionary

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -10,18 +10,7 @@ class Solution:
         num_index_dict = dict()
         for index in range(len(nums)):
             num = nums[index]
-            if num in num_index_dict:
-                num_index_dict[num].append(index)
-            else:
-                num_index_dict[num] = [index]
-                
-        for num in num_index_dict.keys():
             num2 = target - num
-            if num == num2:
-                if len(num_index_dict[num])>=2:
-                    return [num_index_dict[num][0], num_index_dict[num][1]]
-            else:                   
-                if num2 in num_index_dict:
-                    index1 = num_index_dict[num][0]
-                    index2 = num_index_dict[target-num][0]
-                    return [index1, index2]
+            if num2 in num_index_dict:
+                return [num_index_dict[num2], index]
+            num_index_dict[num] = index
